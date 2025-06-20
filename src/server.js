5e8daa066968eddfad57dce453a1b078e4a357dc
Panchenko Denis <Pandenisko@gmail.com>
2025-06-20 21:23:47 +0300

    fin

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
+import * as fs from 'node:fs';
+import path from 'node:path';
 import express from 'express';
 import cors from 'cors'
 import PinoHttp from "pino-http";
-import path from 'node:path'
+import swaggerUI from 'swagger-ui-express';
+
 
 import routes from './routes/routes.js';
 import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 export async function setupServer() {
+    const SWAGGER_DOCUMENT = JSON.parse(
+        fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+    );
     try {
     const app = express();
     const PORT = process.env.PORT || 3000;
@@ -15,10 +21,11 @@ export async function setupServer() {
     app.use(cors({origin: '*'}));
     app.use(PinoHttp());
     app.use(cookieParser());
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCUMENT));
     app.use('/photos', express.static(path.resolve('src', 'uploads', 'photos')));
     app.use('/', routes);
 
-
+    
     app.use('', notFoundHandler);
     app.use(errorHandler);
 
